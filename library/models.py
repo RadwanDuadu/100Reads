85f8f9142f030599b291c176a63c46f59814c881
RadwanDuadu <mradwan444@gmail.com>
2025-10-06 01:13:33 +0100

    Add model for library app

--- library/models.py
+++ library/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+STATUS = (
+    (0, "Draft"),
+    (1, "Published"),
+)
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    author_name = models.CharField(max_length=255)  # Not tied to Django User
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
