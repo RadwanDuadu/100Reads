94ba10561a11b2a6d4d13c1816c1b11f2ead20fe
RadwanDuadu <mradwan444@gmail.com>
2025-10-06 09:32:35 +0100

    Add json file with 9 books

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Book, Review
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'published_on',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
+
 admin.site.register(Review)
