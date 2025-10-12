4cad8391b13473431c831e5bb9b945d68f3bfcbd
RadwanDuadu <mradwan444@gmail.com>
2025-10-12 20:41:51 +0100

    Update css style

--- library/views.py
+++ library/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Book
 
+
 # Create your views here.
 class BookList(generic.ListView):
     queryset = Book.objects.all()
     template_name = "library/index.html"
     paginate_by = 6
+
 
 def book_detail(request, slug):
     """
@@ -22,7 +24,7 @@ def book_detail(request, slug):
     :template:`library/book_detail.html`
     """
 
-    queryset = Book.objects.filter(status=1)
+    queryset = Book.objects.all()
     book = get_object_or_404(queryset, slug=slug)
 
     return render(
