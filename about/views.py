8fc9fd4475823e3d3af89e92b247544589957ee2
RadwanDuadu <mradwan444@gmail.com>
2025-10-14 00:30:27 +0100

    Add contact section to about app

--- about/views.py
+++ about/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .models import About
-
+from .forms import ContactForm
 
 def about_me(request):
     """
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    contact_form = ContactForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+         "contact_form": contact_form
+         },
     )
