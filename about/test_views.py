545b78676720bc52343c554e8b81037a438568ce
RadwanDuadu <mradwan444@gmail.com>
2025-10-25 17:32:59 +0100

    Add  unit test for sucessful contact request  addition

--- about/test_views.py
+++ about/test_views.py
@@ -13,9 +13,21 @@ class TestAboutView(TestCase):
         self.about_content.save()
 
     def test_render_about_page_with_contact_form(self):
-        """Verifies get request for about me containing a collaboration form"""
+        """Verifies get request for about me containing a contact form"""
         response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'About Me', response.content)
         self.assertIsInstance(
             response.context['contact_form'], ContactForm)
+        
+    def test_successful_contact_request_submission(self):
+        """Test for a user requesting a contact"""
+        post_data = {
+            'name': 'test name',
+            'email': 'test@email.com',
+            'message': 'test message'
+        }
+        response = self.client.post(reverse('about'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Contact request received! I endeavour to respond within 2 working days.', response.content)
