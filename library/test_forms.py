129cd7e1ed87952e4ba7252c0e22299efd30f94b
RadwanDuadu <mradwan444@gmail.com>
2025-11-01 00:01:42 +0000

    Update the read list  python files to fix errors

--- library/test_forms.py
+++ library/test_forms.py
@@ -5,13 +5,31 @@ from .forms import ReviewForm
 class TestReviewForm(TestCase):
 
     def test_form_is_valid(self):
-        review_form = ReviewForm({'body': 'This is a great book!', 'rating': 5})
-        self.assertTrue(review_form.is_valid(), msg='Review Form is not valid')
- 
+        review_form = ReviewForm({
+            'body': 'This is a great book!',
+            'rating': 5
+        })
+        self.assertTrue(
+            review_form.is_valid(),
+            msg='Review Form is not valid'
+        )
+
     def test_body_is_invalid(self):
-        review_form = ReviewForm({'body': '', 'rating': 3})
-        self.assertFalse(review_form.is_valid(), msg="Review is missing body, Form is valid")
+        review_form = ReviewForm({
+            'body': '',
+            'rating': 3
+        })
+        self.assertFalse(
+            review_form.is_valid(),
+            msg="Review is missing body, Form is valid"
+        )
 
     def test_rating_is_invalid(self):
-        review_form = ReviewForm({'body': 'This is a bad book', 'rating': ''})
-        self.assertFalse(review_form.is_valid(), msg="Review is missing body, Form is valid")
+        review_form = ReviewForm({
+            'body': 'This is a bad book',
+            'rating': ''
+        })
+        self.assertFalse(
+            review_form.is_valid(),
+            msg="Review is missing rating, Form is valid"
+        )
